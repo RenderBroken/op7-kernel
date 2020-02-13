90c8896793850cc0b893d17261a8c778d5e9df89
Zachariah Kennedy <zkennedy87@gmail.com>
2020-02-13 18:44:39 +0000

    sched/fair.c: Disable CAF's placement_boost with regards to task placement
    Since we use stune boost with CAF's powerhal/sched_boost, lets
    remove placement_boost from the task placement algorythm.
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8190,7 +8190,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	struct cpumask *rtg_target = find_rtg_target(p);
 	struct find_best_target_env fbt_env;
 	bool need_idle = wake_to_idle(p);
-	int placement_boost = task_boost_policy(p);
+	int placement_boost = 0;
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
 	int boosted = (schedtune_task_boost(p) > 0);
