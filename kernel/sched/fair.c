420ce5260893b541adb9f32463816b23cceb9fdf
Zachariah Kennedy <zkennedy87@gmail.com>
2020-03-12 04:34:10 +0000

    sched/fair: Add back overutilization condition for sync tasks
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5827,7 +5827,7 @@ static inline bool
 bias_to_waker_cpu(struct task_struct *p, int cpu, int start_cpu)
 {
 	bool base_test = cpumask_test_cpu(cpu, &p->cpus_allowed) &&
-			cpu_active(cpu);
+			cpu_active(cpu) && !__cpu_overutilized(cpu, task_util(p));
 	bool start_cap_test = (capacity_orig_of(cpu) >=
 					capacity_orig_of(start_cpu));
 
